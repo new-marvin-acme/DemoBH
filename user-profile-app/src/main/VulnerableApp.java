03d8de5d4f955613316a85228f51266d55e22a8f
xygenibot <xygeni_scan@xygeni.io>
2025-12-02 19:41:30 +0000

    Fix java.sql_injection issues in user-profile-app/src/main/VulnerableApp.java and line 15

--- user-profile-app/src/main/VulnerableApp.java
+++ user-profile-app/src/main/VulnerableApp.java
@@ -1,25 +1,25 @@
-import java.sql.*;
-import java.util.Scanner;
-
-public class VulnerableApp {
-
-    public static void main(String[] args) {
-        System.out.print("Enter email to search: ");
-        String email = args[0];
-
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
-             Statement stmt = conn.createStatement()) {
-
-            // Vulnerable SQL statement
-            String query = "SELECT * FROM users WHERE email = '" + email + "'";
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                System.out.println("User: " + rs.getString("name") + " - " + rs.getString("email"));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-}
+1	import java.sql.*;
+2	import java.util.Scanner;
+3	
+4	public class VulnerableApp {
+5	
+6	    public static void main(String[] args) {
+7	        System.out.print("Enter email to search: ");
+8	        String email = args[0];
+9	
+10	        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+11	             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users WHERE email = ?")) {
+12	
+13	            // Secure SQL statement using PreparedStatement
+14	            pstmt.setString(1, email);
+15	            ResultSet rs = pstmt.executeQuery();
+16	
+17	            while (rs.next()) {
+18	                System.out.println("User: " + rs.getString("name") + " - " + rs.getString("email"));
+19	            }
+20	
+21	        } catch (SQLException e) {
+22	            e.printStackTrace();
+23	        }
+24	    }
+25	}
